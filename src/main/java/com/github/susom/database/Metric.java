a3afac2a1d701774566fc358042bbacc51bbfddc
Garrick Olson <garricko@stanford.edu>
2015-09-23 21:54:51 -0700

    Improve documentation for Metric and ensure the message will not contain spaces or tabs

--- src/main/java/com/github/susom/database/Metric.java
+++ src/main/java/com/github/susom/database/Metric.java
@@ -40,6 +40,29 @@ public class Metric {
     }
   }
 
+  /**
+   * <p>Create a metric tracking object and start the nanosecond timer. Times
+   * are obtained using {@code System.nanoTime()}. The canonical way to use
+   * this looks something like this:
+   * </p>
+   *
+   * <pre>
+   *   Metric metric = new Metric(log.isDebugEnabled);
+   *   ...
+   *   metric.checkpoint("received");
+   *   ...
+   *   metric.checkpoint("processed");
+   *   ...
+   *   metric.done("sent");
+   *   ...
+   *   if (log.isDebugEnabled()) {
+   *    log.debug("Processed: " + metric.getMessage());
+   *   }
+   * </pre>
+   *
+   * @param enabled {@code true} if timings will be taken, {@code false} to
+   *                optimize out the time tracking
+   */
   public Metric(boolean enabled) {
     this.enabled = enabled;
     if (enabled) {
@@ -49,6 +72,12 @@ public class Metric {
     }
   }
 
+  /**
+   * Find out how many milliseconds have elapsed since this timer was started.
+   *
+   * @return the number of milliseconds elapsed, or -1 if {@code false} was
+   *         passed in the constructor
+   */
   public long elapsedMillis() {
     if (!enabled) {
       return -1;
@@ -56,19 +85,52 @@ public class Metric {
     return (System.nanoTime() - startNanos) / 1000000;
   }
 
+  /**
+   * Find out how many nanoseconds have elapsed since this timer was started.
+   *
+   * @return the number of nanoseconds elapsed, or -1 if {@code false} was
+   *         passed in the constructor
+   */
+  public long elapsedNanos() {
+    if (!enabled) {
+      return -1;
+    }
+    return (System.nanoTime() - startNanos);
+  }
+
+  /**
+   * Set a mark for timing.
+   *
+   * @param description a label for this mark; may not be null; spaces
+   *                    and tabs will be converted to underscores
+   */
   public void checkpoint(String description) {
     if (enabled) {
       long currentCheckpointNanos = System.nanoTime();
-      checkpoints.add(new Checkpoint(description, currentCheckpointNanos - lastCheckpointNanos));
+      checkpoints.add(new Checkpoint(noTabsOrSpaces(description), currentCheckpointNanos - lastCheckpointNanos));
       lastCheckpointNanos = currentCheckpointNanos;
     }
   }
 
+  /**
+   * Set a final mark for timing and stop the timer.
+   *
+   * @param description a label for this mark; may not be null; spaces
+   *                    and tabs will be converted to underscores
+   * @return time in nanoseconds from the start of this metric, or -1
+   *         if {@code false} was passed in the constructor
+   */
   public long done(String description) {
     checkpoint(description);
     return done();
   }
 
+  /**
+   * Indicate we are done (stop the timer).
+   *
+   * @return time in nanoseconds from the start of this metric, or -1
+   *         if {@code false} was passed in the constructor
+   */
   public long done() {
     if (enabled) {
       lastCheckpointNanos = System.nanoTime();
@@ -77,6 +139,15 @@ public class Metric {
     return -1;
   }
 
+  /**
+   * Construct and return a message based on the timing and checkpoints. This
+   * will look like "123.456ms(checkpoint1=100.228ms,checkpoint2=23.228ms)"
+   * without the quotes. There will be no spaces or tabs in the output.
+   *
+   * @return a string with timing information, or {@code "metricsDisabled"}
+   *         if {@code false} was passed in the constructor.
+   * @see #printMessage(StringBuilder)
+   */
   public String getMessage() {
     if (enabled) {
       StringBuilder buf = new StringBuilder();
@@ -86,6 +157,16 @@ public class Metric {
     return "metricsDisabled";
   }
 
+  /**
+   * Construct and print a message based on the timing and checkpoints. This
+   * will look like "123.456ms(checkpoint1=100.228ms,checkpoint2=23.228ms)"
+   * without the quotes. There will be no spaces or tabs in the output. A
+   * value of {@code "metricsDisabled"} will be printed if {@code false} was
+   * passed in the constructor.
+   *
+   * @param buf the message will be printed to this builder
+   * @see #getMessage()
+   */
   public void printMessage(StringBuilder buf) {
     if (enabled) {
       writeNanos(buf, lastCheckpointNanos - startNanos);
@@ -126,5 +207,9 @@ public class Metric {
       }
     }
     buf.append("ms");
+  }
+
+  private String noTabsOrSpaces(String s) {
+    return s.replace(' ', '_').replace('\t', '_');
   }
 }

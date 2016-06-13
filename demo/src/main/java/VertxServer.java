85ef0139bdb200eb2b9ead806df3c2b67e4d8f3b
Garrick Olson <garricko@stanford.edu>
2016-06-13 13:43:25 -0700

    Clean up the Vert.x sample and fix metrics and logging

--- demo/src/main/java/VertxServer.java
+++ demo/src/main/java/VertxServer.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,19 +15,13 @@ import io.vertx.core.json.JsonObject;
  */
 public class VertxServer {
   private static final Logger log = LoggerFactory.getLogger(VertxServer.class);
-  private static final Object lock = new Object();
+  private final Object lock = new Object();
 
   public void run() throws Exception {
     // A JSON config you might get from Vertx. In a real scenario you would
-    // also set database.user, database.password, and database.pool.size here.
+    // also set database.user, database.password and database.pool.size.
     JsonObject jsonConfig = new JsonObject()
-        .put("database.url", "jdbc:derby:target/testdb;create=true");
-
-//    JsonObject jsonConfig = new JsonObject()
-//        .put("database.url", "jdbc:postgresql://localhost/vagrant")
-//        .put("database.user", "vagrant")
-//        .put("database.password", "vagrant")
-//        .put("database.pool.size", "5");
+        .put("database.url", "jdbc:hsqldb:file:target/hsqldb;shutdown=true");
 
     // Set up Vertx and database access
     Vertx vertx = Vertx.vertx();
@@ -44,10 +36,10 @@ public class VertxServer {
       new Schema().addTable("t")
           .addColumn("pk").primaryKey().table()
           .addColumn("message").asString(80).schema().execute(db);
+
       db.get().toInsert("insert into t (pk,message) values (?,?)")
-          .argInteger(1).argString("Hello World!").insert(1);
-      db.get().toInsert("insert into t (pk,message) values (?,?)")
-          .argInteger(2).argString("Goodbye!").insert(1);
+          .argInteger(1).argString("Hello World!").batch()
+          .argInteger(2).argString("Goodbye!").insertBatch();
     });
 
     // Start our server
@@ -61,9 +53,7 @@ public class VertxServer {
         request.response().setStatusCode(404).end();
         return;
       }
-
       int pk = Integer.parseInt(pkParam);
-      metric.checkpoint("read");
 
       // Lookup the message from the database
       dbb.transactAsync(db -> {
@@ -75,31 +65,33 @@ public class VertxServer {
       }, result -> {
         // Now we are back on the event loop thread
         metric.checkpoint("result");
+        request.response().bodyEndHandler(h -> {
+          metric.done("sent", request.response().bytesWritten());
+          log.debug("Served request: " + metric.getMessage());
+        });
         if (result.succeeded()) {
           request.response().setStatusCode(200).putHeader("content-type", "text/plain").end(result.result());
         } else {
           log.error("Returning 500 to client", result.cause());
           request.response().setStatusCode(500).end();
         }
-        metric.done("sent");
-        log.debug("Served request: " + metric.getMessage());
       });
     }).listen(8123, result ->
-        println("Started server. Go to http://localhost:8123/?pk=1 or http://localhost:8123/?pk=2")
+        log.info("Started server. Go to http://localhost:8123/?pk=1 or http://localhost:8123/?pk=2")
     );
 
     // Attempt to do a clean shutdown on JVM exit
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      println("Trying to stop the server nicely");
+      log.debug("Trying to stop the server nicely");
       try {
         synchronized (lock) {
           // First shutdown Vert.x
           vertx.close(h -> {
-            println("Vert.x stopped, now closing the connection pool");
+            log.debug("Vert.x stopped, now closing the connection pool");
             synchronized (lock) {
               // Then shutdown the database pool
               dbb.close();
-              println("Server stopped");
+              log.debug("Server stopped");
               lock.notify();
             }
           });
@@ -111,18 +103,8 @@ public class VertxServer {
     }));
   }
 
-  public void println(String s) {
-    System.out.println(s);
-  }
-
   public static void main(final String[] args) {
     try {
-      // Put all Derby related files inside ./target to keep our working copy clean
-      File directory = new File("target").getAbsoluteFile();
-      if (directory.exists() || directory.mkdirs()) {
-        System.setProperty("derby.stream.error.file", new File(directory, "derby.log").getAbsolutePath());
-      }
-
       new VertxServer().run();
     } catch (Exception e) {
       e.printStackTrace();

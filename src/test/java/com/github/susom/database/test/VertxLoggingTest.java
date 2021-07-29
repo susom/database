cf0156fd20a47119e661f6639134f1a5f939ffb1
Glenna <glennamayo@gmail.com>
2021-07-29 16:31:50 -0700

    Susom DB on Vert.x 4 (#61)
    * Susom DB on Vert.x 4
    
    * Tidy a bit, adjust scope for vertx-web-client, and bump more lib versions
    
    Co-authored-by: Glenna Mayo <glennam@stanford.edu>
    Co-authored-by: Garrick Olson <garricko@stanford.edu>

--- src/test/java/com/github/susom/database/test/VertxLoggingTest.java
+++ src/test/java/com/github/susom/database/test/VertxLoggingTest.java
@@ -1,23 +1,22 @@
 package com.github.susom.database.test;
 
+import com.github.susom.database.Config;
+import com.github.susom.database.ConfigFrom;
+import com.github.susom.database.DatabaseProviderVertx;
+import com.github.susom.database.DatabaseProviderVertx.Builder;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.WebClient;
 import java.io.File;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-
-import com.github.susom.database.Config;
-import com.github.susom.database.ConfigFrom;
-import com.github.susom.database.DatabaseProviderVertx;
-import com.github.susom.database.DatabaseProviderVertx.Builder;
-
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 /**
  * Make sure the SLF4J MDC feature behaves well with Vertx.
@@ -45,51 +44,57 @@ public class VertxLoggingTest {
     Builder db = DatabaseProviderVertx.pooledBuilder(vertx, config).withSqlParameterLogging();
 
     vertx.createHttpServer()
-        .requestHandler(r -> {
-          Context context1 = Vertx.currentContext();
-          log.debug("Request before blocking1: " + context1);
-          db.transactAsync(dbs -> {
-            Context context2 = Vertx.currentContext();
-            log.debug("Request inside blocking2: " + context2);
-            context.assertEquals(context1, context2);
-            return null;
-          }, result -> {
-            Context context3 = Vertx.currentContext();
-            log.debug("Request after blocking3: " + context3);
-            context.assertEquals(context1, context3);
+      .requestHandler(r -> {
+        Context context1 = Vertx.currentContext();
+        log.debug("Request before blocking1: " + context1);
+        db.transactAsync(dbs -> {
+          Context context2 = Vertx.currentContext();
+          log.debug("Request inside blocking2: " + context2);
+          context.assertEquals(context1, context2);
+          return null;
+        }, result -> {
+          Context context3 = Vertx.currentContext();
+          log.debug("Request after blocking3: " + context3);
+          context.assertEquals(context1, context3);
 
+          db.transactAsync(dbs -> {
+            Context context4 = Vertx.currentContext();
+            log.debug("Request inside blocking4: " + context4);
+            context.assertEquals(context1, context4);
+            return null;
+          }, result2 -> {
+            Context context5 = Vertx.currentContext();
+            log.debug("Request after blocking5: " + context5);
+            context.assertEquals(context1, context5);
             db.transactAsync(dbs -> {
-              Context context4 = Vertx.currentContext();
-              log.debug("Request inside blocking4: " + context4);
-              context.assertEquals(context1, context4);
+              Context context6 = Vertx.currentContext();
+              log.debug("Request inside blocking6: " + context6);
+              context.assertEquals(context1, context6);
               return null;
-            }, result2 -> {
-              Context context5 = Vertx.currentContext();
-              log.debug("Request after blocking5: " + context5);
-              context.assertEquals(context1, context5);
-              db.transactAsync(dbs -> {
-                Context context6 = Vertx.currentContext();
-                log.debug("Request inside blocking6: " + context6);
-                context.assertEquals(context1, context6);
-                return null;
-              }, result3 -> {
-                Context context7 = Vertx.currentContext();
-                log.debug("Request after blocking7: " + context7);
-                context.assertEquals(context1, context7);
-                r.response().end();
-                MDC.clear();
-                async.complete();
-              });
-              MDC.put("userId", "bob");
+            }, result3 -> {
+              Context context7 = Vertx.currentContext();
+              log.debug("Request after blocking7: " + context7);
+              context.assertEquals(context1, context7);
+              r.response().end();
+              MDC.clear();
+              async.complete();
             });
             MDC.put("userId", "bob");
           });
-        }).listen(8111, server ->
-          vertx.createHttpClient().get(8111, "localhost", "/foo").handler(response -> {
-            context.assertEquals(200, response.statusCode());
-            vertx.close();
-          }).end()
-        );
+          MDC.put("userId", "bob");
+        });
+      }).listen(8111, server -> {
+
+      WebClient client = WebClient.create(vertx);
+      client.get(8111, "localhost", "/foo").send()
+        .onSuccess(response -> context.assertEquals(200, response.statusCode()))
+        .onFailure(err -> context.fail(err.getMessage()))
+        .eventually(res -> {
+          client.close();
+          vertx.close();
+          return Future.succeededFuture(Boolean.TRUE);
+        });
+    });
   }
 
   @Test
@@ -102,55 +107,62 @@ public class VertxLoggingTest {
     Builder db = DatabaseProviderVertx.pooledBuilder(vertx, config).withSqlParameterLogging();
 
     vertx.createHttpServer()
-        .requestHandler(r -> {
-          context.assertTrue(MDC.getCopyOfContextMap() == null || MDC.getCopyOfContextMap().isEmpty(),
-              "Was: " + MDC.getCopyOfContextMap());
-          MDC.put("userId", "joe");
-          MDC.put("requestId", "1");
-          log.debug("Request before blocking: " + MDC.getCopyOfContextMap());
+      .requestHandler(r -> {
+        context.assertTrue(MDC.getCopyOfContextMap() == null || MDC.getCopyOfContextMap().isEmpty(),
+          "Was: " + MDC.getCopyOfContextMap());
+        MDC.put("userId", "joe");
+        MDC.put("requestId", "1");
+        log.debug("Request before blocking: " + MDC.getCopyOfContextMap());
+        db.transactAsync(dbs -> {
+          log.debug("Request inside blocking: " + MDC.getCopyOfContextMap());
+          context.assertEquals("joe", MDC.get("userId"));
+          context.assertEquals("1", MDC.get("requestId"));
+          return null;
+        }, result -> {
+          log.debug("Request after blocking: " + MDC.getCopyOfContextMap());
+          context.assertEquals("joe", MDC.get("userId"));
+          context.assertEquals("1", MDC.get("requestId"));
+
           db.transactAsync(dbs -> {
-            log.debug("Request inside blocking: " + MDC.getCopyOfContextMap());
+            log.debug("Nested request inside blocking: " + MDC.getCopyOfContextMap());
             context.assertEquals("joe", MDC.get("userId"));
             context.assertEquals("1", MDC.get("requestId"));
             return null;
-          }, result -> {
-            log.debug("Request after blocking: " + MDC.getCopyOfContextMap());
+          }, result2 -> {
+            log.debug("Nested request after blocking: " + MDC.getCopyOfContextMap());
             context.assertEquals("joe", MDC.get("userId"));
             context.assertEquals("1", MDC.get("requestId"));
-
             db.transactAsync(dbs -> {
-              log.debug("Nested request inside blocking: " + MDC.getCopyOfContextMap());
+              log.debug("Nested2 request inside blocking: " + MDC.getCopyOfContextMap());
               context.assertEquals("joe", MDC.get("userId"));
               context.assertEquals("1", MDC.get("requestId"));
               return null;
-            }, result2 -> {
-              log.debug("Nested request after blocking: " + MDC.getCopyOfContextMap());
+            }, result3 -> {
+              log.debug("Nested2 request after blocking: " + MDC.getCopyOfContextMap());
               context.assertEquals("joe", MDC.get("userId"));
               context.assertEquals("1", MDC.get("requestId"));
-              db.transactAsync(dbs -> {
-                log.debug("Nested2 request inside blocking: " + MDC.getCopyOfContextMap());
-                context.assertEquals("joe", MDC.get("userId"));
-                context.assertEquals("1", MDC.get("requestId"));
-                return null;
-              }, result3 -> {
-                log.debug("Nested2 request after blocking: " + MDC.getCopyOfContextMap());
-                context.assertEquals("joe", MDC.get("userId"));
-                context.assertEquals("1", MDC.get("requestId"));
-                r.response().end();
-                MDC.clear();
-              });
-              MDC.put("userId", "bob");
+              r.response().end();
+              MDC.clear();
             });
             MDC.put("userId", "bob");
           });
+          MDC.put("userId", "bob");
+        });
 
-          // Clear the MDC when we are done (the async stuff should track/restore as needed)
-          MDC.clear();
-        }).listen(8101, server ->
-          vertx.createHttpClient().get(8101, "localhost", "/foo").handler(response ->
-            context.assertEquals(200, response.statusCode())
-          ).end()
-        );
+        // Clear the MDC when we are done (the async stuff should track/restore as needed)
+        MDC.clear();
+      }).listen(8101, server -> {
+
+        WebClient client = WebClient.create(vertx);
+        client.get(8101, "localhost", "/foo").send()
+          .onSuccess(response -> context.assertEquals(200, response.statusCode()))
+          .onFailure(err -> context.fail(err.getMessage()))
+          .eventually((res -> {
+            client.close();
+            return Future.succeededFuture(Boolean.TRUE);
+          }));
+      }
+    );
 
     vertx.setPeriodic(100, id -> {
       // Repeat enough times to cycle through all workers
@@ -158,7 +170,7 @@ public class VertxLoggingTest {
         async.complete();
       }
       context.assertTrue(MDC.getCopyOfContextMap() == null || MDC.getCopyOfContextMap().isEmpty(),
-          "Was: " + MDC.getCopyOfContextMap());
+        "Was: " + MDC.getCopyOfContextMap());
       MDC.put("userId", "<timer>");
       log.debug("Timer before blocking: " + MDC.getCopyOfContextMap());
       db.transactAsync(dbs -> {
@@ -210,4 +222,5 @@ public class VertxLoggingTest {
       MDC.clear();
     });
   }
+
 }

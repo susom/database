17b3e8d4dee00cd5dacfd3142a4d837da92cc7d1
Garrick Olson <garricko@stanford.edu>
2021-08-20 00:16:57 -0700

    Improve database version testing and coverage reporting in SonarCloud
    Remove Oracle shim now that drivers are in Maven central

---
+++ test-oracle.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PASSWORD=$(openssl rand -base64 18 | tr -d +/)
+#export TZ=Asia/Kolkata
+export TZ=America/Los_Angeles
+
+ORACLE_NAME=dbtest-ora
+
+stop_oracle() {
+  docker rm -f $ORACLE_NAME
+}
+
+start_oracle() {
+  # Supposedly we could set -e ORACLE_PWD=$PASSWORD here, but it doesn't seem to work
+  docker run -d --rm --name $ORACLE_NAME --pull always -p 1521:1521 -p 5500:5500 us-west1-docker.pkg.dev/som-rit-infrastructure-prod/third-party/oracledb:$1
+
+  if [ $? -ne 0 ] ; then
+    echo "Unable to start Oracle docker ($1)"
+    exit 1
+  fi
+
+  declare -i count=1
+  while [  "$(docker inspect --format='{{json .State.Health.Status}}' $ORACLE_NAME)" != '"healthy"' ]
+  do
+    echo "Waiting for container to start ($count seconds)"
+    sleep 1
+
+    count=$((count + 1))
+    if [ $count -gt 120 ] ; then
+      echo "Database did not startup correctly ($1)"
+      stop_oracle
+      exit 1
+    fi
+  done
+
+  docker cp oracledb.sql dbtest-ora:/home/oracle/oracledb.sql
+  docker exec -it $ORACLE_NAME sqlplus / AS SYSDBA @/home/oracle/oracledb.sql
+}
+
+test_oracle() {
+  mvn -e -Dmaven.javadoc.skip=true \
+      -Dfailsafe.rerunFailingTestsCount=2 \
+      "-Ddatabase.url=jdbc:oracle:thin:@localhost:1521:ORCLCDB" \
+      -Ddatabase.user=testuser \
+      -Ddatabase.password="TestPassword456" \
+      -P oracle19.only,coverage test
+}
+
+run_oracle_tests() {
+  start_oracle $1
+
+  test_oracle
+
+  if [ $? -ne 0 ] ; then
+    echo "Test mvn command failed"
+    stop_oracle
+    exit 1
+  fi
+
+  stop_oracle
+}
+
+run_oracle_tests "19.3-quick"
+#run_oracle_tests 2019-latest
---
+++ test-sqlserver.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+PASSWORD=$(openssl rand -base64 18 | tr -d +/)
+#export TZ=Asia/Kolkata
+export TZ=America/Los_Angeles
+
+run_ms_tests() {
+  docker run -d --rm --name dbtest-ms --pull always -e ACCEPT_EULA=Y -e TZ=$TZ -e SA_PASSWORD=$PASSWORD -p 1433:1433 --health-cmd='/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P '$PASSWORD' -Q "SELECT 1"' --health-interval=2s --health-timeout=30s --health-retries=5 mcr.microsoft.com/mssql/server:$1
+
+  declare -i count=1
+  while [  "$(docker inspect --format='{{json .State.Health.Status}}' dbtest-ms)" != '"healthy"' ]
+  do
+    echo "Waiting for container to start ($count seconds)"
+    sleep 1
+
+    count=$((count + 1))
+    if [ $count -gt 120 ] ; then
+      echo "Database did not startup correctly ($1)"
+      docker rm -f dbtest-ms
+      exit 1
+    fi
+  done
+
+  mvn -e -Dmaven.javadoc.skip=true \
+      -Dfailsafe.rerunFailingTestsCount=2 \
+      "-Dsqlserver.database.url=jdbc:sqlserver://localhost:1433" \
+      -Dsqlserver.database.user=sa \
+      -Dsqlserver.database.password=$PASSWORD \
+      -P sqlserver.only,coverage test
+
+  if [ $? -ne 0 ] ; then
+    echo "mvn command failed"
+    docker rm -f dbtest-ms
+    exit 1
+  fi
+
+  docker rm -f dbtest-ms
+}
+
+run_ms_tests 2017-latest
+run_ms_tests 2019-latest

10e8ded105da9c7d59c249aef0c1fa3ca3bffad4
kcs-bandihareesh <bandi.hareesh@kcsitglobal.com>
2024-10-28 20:31:00 +0530

    PostgreSQL with pg_isready check

--- test-postgres.sh
+++ test-postgres.sh
@@ -9,7 +9,7 @@ run_pg_tests() {
   docker run -d --rm --name dbtest-pg -e TZ=$TZ -e POSTGRES_PASSWORD=$PASSWORD -p 5432:5432 postgres:$1
 
   declare -i count=1
-  while [ "$(docker inspect --format='{{.State.Health.Status}}' dbtest-pg 2>/dev/null)" != "healthy" ] && [ $count -le 120 ];
+  until docker exec dbtest-pg pg_isready -U postgres -h localhost -p 5432 > /dev/null 2>&1;
   do
     echo "Waiting for container to start ($count seconds)"
     sleep 1
